b2a2a929336ef1638304729cef69ff8b247f8578
Fernando Mateo <fmateoe@ext.laligatech.com>
2023-10-31 11:47:44 +0100

    stablished all react query to upluad the file

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// will map to /api/create-chat
+export async function POST(req: Request, res: Response) {
+  try {
+    const body = await req.json();
+    const { file_key, file_name } = body;
+    console.log(file_key, file_name);
+    return NextResponse.json({message: "success"})
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
